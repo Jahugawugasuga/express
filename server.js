f89fcfa972b6b392ad916cf129a7f748145d7053
James Brooks <60955280+Jahugawugasuga@users.noreply.github.com>
2020-05-13 18:17:36 -0600

    added functionality - add notes to db

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ var notesData = require("./db/db.json")
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
+// express app to handle data parsing of user inputs
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); //do we even need this? usually with integers 
+app.use(express.json()); 
+app.use(express.static('public')); 
 
 //html routes
 
@@ -22,22 +23,22 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+// api routes
 app.get("/api/notes", function(req, res) {
     return res.json(notesData)
 });
 
 
-
-
-
-//
+//add new note from user input to be stored
 app.post("/api/notes", function(req, res) {
 
      var userNote = req.body;
 
+     userNote.id = Date.now()
+
     notesData.push(userNote);
 
-//add note to database file    
+//add new data to database in existing database file    
 fs.writeFile("./db/db.json", JSON.stringify(notesData), "utf8", function errorCatch(err, data) {
 if (err) {
     console.log (err);
@@ -46,16 +47,29 @@ if (err) {
 })
       res.json(userNote);
   })
+//delete notes based on id in the database 
+app.delete(`/api/notes/:id`, function (req, res) {
+    
+    var chosen = req.params.id;
+    console.log(req.params.id, "I was clicked"); //this works
+    fs.readFile(__dirname + "/db/db.json", "utf8", function errorCatch (err, data) {
+        if (err) {
+            console.log (err); // no error 
+        }
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-//   app.post("/api/clear", function(req, res) { //DELETE NOTES?
-//     // Empty out the arrays of data
-//     notes.length = 0;
-//     res.json({ ok: true });
-//   });
+        var obj = JSON.parse(data)
+       console.log(obj);
+       console.log(obj.id) //this is undefined
+        for (var i =0; i<obj.length; i++) {
+            console.log ("im here fatty")
+            if (obj[i].id = chosen) {
+                obj.splice(i,1)
+                console.log(chosen, "I was chosen!") //not working
+                console.log(obj, "I am the obj!"); //not working
+            }
+        }
+    })
+})
 
 //feedback to user that server is active
 app.listen(PORT, function() {
