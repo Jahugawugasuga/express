2ee870d180ab0eee5ce67f00ba9f0262d0b0849a
James Brooks <60955280+Jahugawugasuga@users.noreply.github.com>
2020-05-14 19:02:19 -0600

    added filter to delete
    i can add notes to the object but still wont delete.

--- server.js
+++ server.js
@@ -9,69 +9,71 @@ var PORT = process.env.PORT || 3000;
 
 // express app to handle data parsing of user inputs
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); 
-app.use(express.static('public')); 
+app.use(express.json());
+app.use(express.static('public'));
 
 //html routes
 
-app.get("/notes", function(req, res) {
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 
 });
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 // api routes
-app.get("/api/notes", function(req, res) {
+app.get("/api/notes", function (req, res) {
     return res.json(notesData)
 });
 
 
 //add new note from user input to be stored
-app.post("/api/notes", function(req, res) {
+app.post("/api/notes", function (req, res) {
 
-     var userNote = req.body;
+    var userNote = req.body;
 
-     userNote.id = Date.now()
+    userNote.id = Date.now()
 
     notesData.push(userNote);
 
-//add new data to database in existing database file    
-fs.writeFile("./db/db.json", JSON.stringify(notesData), "utf8", function errorCatch(err, data) {
-if (err) {
-    console.log (err);
-}
+    //add new data to database in existing database file    
+    fs.writeFile("./db/db.json", JSON.stringify(notesData), "utf8", function (err, data) {
+        if (err) {
+            console.log(err);
+        }
 
+    })
+    res.json(userNote);
 })
-      res.json(userNote);
-  })
 //delete notes based on id in the database 
 app.delete(`/api/notes/:id`, function (req, res) {
-    
-    var chosen = req.params.id;
-    console.log(req.params.id, "I was clicked"); //this works
-    fs.readFile(__dirname + "/db/db.json", "utf8", function errorCatch (err, data) {
-        if (err) {
-            console.log (err); // no error 
-        }
 
-        var obj = JSON.parse(data)
-       console.log(obj);
-       console.log(obj.id) //this is undefined
-        for (var i =0; i<obj.length; i++) {
-            console.log ("im here fatty")
-            if (obj[i].id = chosen) {
-                obj.splice(i,1)
-                console.log(chosen, "I was chosen!") //not working
-                console.log(obj, "I am the obj!"); //not working
-            }
+    var chosen = req.params.id;
+    fs.readFile(path.join(__dirname,"db/db.json"), function (err,data) {
+        if (err) {
+            console.log(err);
         }
+        var newData = JSON.parse (data);
+    var newVar = newData.filter(note => note.id !==chosen)
+ console.log (newVar)
+    fs.writeFile("./db/db.json", JSON.stringify(newVar), "utf8", function (err) {
+        if (err) {
+            console.log(err);
+        }
+        console.log(newVar);
+        console.log(chosen);
+console.log("SUCCESS")
     })
+
 })
 
+
+})
+
+
 //feedback to user that server is active
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
-  });
+});
